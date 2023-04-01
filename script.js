4ba942b034981e83a0075497616729c687ccebb4
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-03-31 19:57:34 -0500

    layed out basic html structure ( start screen, question screen, game over screen, leaderboard

---
+++ script.js
@@ -0,0 +1,6 @@
+// A start button is present, once the user clicks it then a timer starts and the first question appears with multiple choice buttons below it
+//upon clicking 1 of the 4 multple choice buutons, their response is stored and the next question pops up
+//If the users clicks a wrong answer then time is subsracted from the clock
+//when all questions are answered or the timer reaches 0, then the game ends
+//upon the game ending a screen pops up showing high scores and you can enter your initials to save your score on the leaderboard
+
