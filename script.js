886cb9c232c71c3aff5525a8a79dd28b9f9736d4
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-04-01 14:58:42 -0500

    Added my questions and answers, made them into an array of objects for easy access

--- script.js
+++ script.js
@@ -37,4 +37,56 @@ var countdownClock = document.getElementById("countdown");
 });
 
 
+
+
+
+
 });
+
+// Making my  questions, made an array of objects.Easy access to all questions via an index.Made a property "options" array for each questions options.
+Var quizQuestions = 
+[{
+    question: "What does JavaScript do?",
+    options: ["Makes web page interactive","Styles the web page", "Creates basic layout of webpage", "Connects site to internet" ],
+    correctOption: 0
+},
+{
+    question: "What is an object?",
+    options: ["Allows the user to edit the web page", "It is a standalone entity with properties and type.", "Objects are not related to JavaScript", "Any image or video on a web page" ],
+    correctOption: 1
+},
+{
+    question: "What is a function's 'this' keyword?",
+    options: [" 'this' is not used in JavaScript'", "Unique ID", "Keyword that references the object that it is in", "Keyword that indicates which JS file you are using " ],
+    correctOption: 2
+},
+{
+    question: "What is an event listener?",
+    options: ["Looks out for viruses", "Keeps track of web traffic", "Tracks the users internet history","Runs code only once user interacts with it" ],
+    correctOption: 3
+},
+{
+    question: "What are keyboard events?",
+    options: ["Happens when user presses a keyboard key", "Keyboards events are related to CSS, not JavaScript", "They allow you to embed a virual keyboard on the screen", "nothing important" ],
+    correctOption: 0
+},
+{
+    question: "What is event bubbling?",
+    options: ["Just pick an answer", "flexbox", "When element recieves an action, it propagates up", "Cleaning the entire schools water fountains" ],
+    correctOption: 2
+},
+{
+    question: "What is a Data attribute",
+    options: ["Heisenberg", "Quit now", "Something you don't truly understand", " Used to store some extra information that doesn't have any visual representation" ],
+    correctOption: 3
+},
+{
+    question: "What is local storage?",
+    options: ["Memory card", "things", " Web storage API allows you store data as key-value pairs ", "uHaul" ],
+    correctOption: 2
+},
+{
+    question: "What is setting an attribute?",
+    options: ["almost done", " used to set or add an attribute to a particular element and provides a value to it", "good job", "you'll make it" ],
+    correctOption: 1
+}];
